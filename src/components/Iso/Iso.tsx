ba3c583ca857be9a89fac663352e5bf0a7f6398d
歌川智 <hyperecoyaro@gmail.com>
2023-10-22 22:40:46 +0900

    css追加

--- src/components/Iso/Iso.tsx
+++ src/components/Iso/Iso.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import './iso.css';
 
 const Iso: React.FC = () => {
   return (
-    <div>
-      <p>iso page</p>
-      <iframe width="500" height="315" src="https://www.youtube.com/embed/TQL8w8bLAxQ"></iframe>
-      <iframe width="500" height="315" src="https://www.youtube.com/embed/0Lo7X1WIrz8"></iframe>
-      <iframe width="500" height="315" src="https://www.youtube.com/embed/2IzR_ClTE8Y"></iframe>
-      <iframe width="500" height="315" src="https://www.youtube.com/embed/DmQWGH8r3oE"></iframe>
+    <div className="video-container">
+      <p className="page-title">iso page</p>
+      <div className="video-wrapper">
+        <div className="video">
+          <iframe title="v1" src="https://www.youtube.com/embed/TQL8w8bLAxQ"></iframe>
+        </div>
+        <div className="video">
+          <iframe title="v2" src="https://www.youtube.com/embed/0Lo7X1WIrz8"></iframe>
+        </div>
+        <div className="video">
+          <iframe title="v3" src="https://www.youtube.com/embed/2IzR_ClTE8Y"></iframe>
+        </div>
+        <div className="video">
+          <iframe title="v4" src="https://www.youtube.com/embed/DmQWGH8r3oE"></iframe>
+        </div>
+      </div>
     </div>
+    // <div>
+    //   <p>iso page</p>
+    //   <iframe width="500" height="315" title="v1" src="https://www.youtube.com/embed/TQL8w8bLAxQ"></iframe>
+    //   <iframe width="500" height="315" title="v2" src="https://www.youtube.com/embed/0Lo7X1WIrz8"></iframe>
+    //   <iframe width="500" height="315" title="v3" src="https://www.youtube.com/embed/2IzR_ClTE8Y"></iframe>
+    //   <iframe width="500" height="315" title="v4" src="https://www.youtube.com/embed/DmQWGH8r3oE"></iframe>
+    // </div>
   );
 };
 
