228a7a9a6f099e2c2f59ca4b5b06823305c12c01
歌川智 <hyperecoyaro@gmail.com>
2023-10-22 17:34:31 +0900

    test

--- src/components/Iso/Iso.tsx
+++ src/components/Iso/Iso.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Iso: React.FC = () => {
   return (
     <div>
-      <p>iso page </p>
+      <p>1</p>
+      <p>iso page</p>
       <iframe width="500" height="315" src="https://www.youtube.com/embed/TQL8w8bLAxQ"></iframe>
       <iframe width="500" height="315" src="https://www.youtube.com/embed/0Lo7X1WIrz8"></iframe>
       <iframe width="500" height="315" src="https://www.youtube.com/embed/2IzR_ClTE8Y"></iframe>
